7351dd19e0d9cdf7383f551c1eec2b89d902f8af
admen <nikita.maltsev@yandex.ru>
2022-06-27 02:01:50 +0300

    fix

--- src/main/java/com/parser/kladr/services/impl/DefaultGenerateService.java
+++ src/main/java/com/parser/kladr/services/impl/DefaultGenerateService.java
@@ -13,37 +13,64 @@ import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class DefaultGenerateService implements GenerateService {
     @PersistenceContext
     EntityManager entityManager;
 
-    //  private final List<String> socrs = Arrays.asList("п", "г", "с", "д", "пгт");
+    private List<Value> valueList = new ArrayList<>();
 
 
     @Override
     @Transactional
     public void generate(GenerateRequest request) {
-        List<Kladr> ass = entityManager.createQuery("SELECT k from Kladr k where k.socr in :values", Kladr.class)
+        List<Kladr> citys = entityManager.createQuery("SELECT k from Kladr k where k.socr in :values", Kladr.class)
                 .setParameter("values", request.getSocrs()).getResultList();
         long of;
+        int count = 0;
         if (request.getCountGenerate() == -1) {
-            of = ass.size();
+            of = citys.size();
         } else {
             of = request.getCountGenerate();
         }
 
-        for (int i = 0; i <= of; i++) {
-            var inspectedKladr = ass.get(i);
+        for (int i = 0; i < of; i++) {
+            var inspectedKladr = citys.get(i);
             var code = codeSelectGenerator(inspectedKladr.getCode());
             generateValue(inspectedKladr, code);
             StringBuilder builder = new StringBuilder();
             builder.append("Generate: ").append(i).append(" of: ").append(of).append(" / ").append(inspectedKladr.getName());
             System.out.println(builder);
-            entityManager.flush();
-
+            if (valueList.size() == 1000) {
+                System.out.println("Buffer = "+ valueList.size() +" persist and cleaning buffer...");
+                startToPersist();
+                valueList = new ArrayList<>();
+                System.gc();
+                count += 1000;
+            }
         }
+
+        System.out.println("parse complete ... Count values: " + count + valueList.size());
+        startToPersist();
+        System.out.println("all done.");
+    }
+
+    @Transactional
+    void startToPersist() {
+        final int total = valueList.size();
+        AtomicInteger i = new AtomicInteger();
+        valueList.forEach(value ->
+                {
+
+                    System.out.println(i + " / " + total + " persist: " + value);
+                    entityManager.persist(value);
+                    i.getAndIncrement();
+
+                }
+        );
+        entityManager.flush();
     }
 
     @Transactional
@@ -60,16 +87,16 @@ public class DefaultGenerateService implements GenerateService {
                     .streets(streetValue)
                     .build();
 
-             flush(value);
+            valueList.add(value);
 
         }
+        if (streets.size() == 0) {
+            valueList.add(Value.builder()
+                    .city(city)
+                    .streets("-")
+                    .build());
+        }
 
-    }
-
-
-    @Transactional
-    void flush(Value value) {
-        entityManager.persist(value);
     }
 
     private String codeSelectGenerator(String startCode) {
